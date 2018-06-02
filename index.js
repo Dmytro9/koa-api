6d83250ceecdf66a7cbc83abac7cd2852fc8fe63
INNOVECS\dmytro.poliuliuk <Dmytro.Poliuliuk@innovecs.com>
2018-06-02 23:13:35 +0300

    GET POST routes for Users

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-import Koa from 'koa'
-import Router from 'koa-router'
+const Koa = require('koa');
+const Router = require('koa-router');
 
-// const Koa = require('koa');
-const app = new Koa()
+const app = new Koa();
+const router = new Router();
 
-app.use(function* () {
-  this.body = 'Hello World';
+// pretend db
+const users = [
+  {
+    name: 'Dima',
+    email: 'dpoliuliuk@gmail.com',
+  },
+  {
+    name: 'Inna',
+    email: 'dpoliuliuk@gmail.com',
+  },
+  {
+    name: 'Vasia',
+    email: 'dpoliuliuk@gmail.com',
+  },
+]
+
+// routes
+router.get('/user/:id', ctx => {
+  ctx.body = users[ctx.params.id];
 });
+
+router.post('/user/:id', ctx => {
+  ctx.body = Object.assign(users[ctx.params.id], ctx.request.body);
+});
+
+app
+  .use(require('koa-body')())
+  .use(router.allowedMethods())
+  .use(router.routes())
+
 
 app.listen(1337);
